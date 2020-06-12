0a3ab799c6650381a607f4be01f4421158027172
Sonya Gould <sonyavgould@gmail.com>
2020-06-12 04:29:20 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -14,3 +14,21 @@ while row_index < src.count do
   end
 end
 end
+
+
+# outer_results = []
+# row_index = 0
+# while row_index < spice_rack.count do
+#   element_index = 0
+#   while element_index < spice_rack[row_index].count do
+#     # How to read the following line of code:
+#     #   Array at row_index
+#     #   Element of the inner array at element_index
+#     #   The first character of that element...
+#     if spice_rack[row_index][element_index][0] == "P"
+#       outer_results << spice_rack[row_index][element_index]
+#     end
+#     element_index += 1
+#   end
+#   row_index += 1
+# end
