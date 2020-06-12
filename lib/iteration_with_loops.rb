397418268f964e1fdaf4cb82b24daf7893ba6381
Sonya Gould <sonyavgould@gmail.com>
2020-06-12 04:30:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,10 +8,10 @@ while row_index < src.count do
     if src[row_index][element_index] < smallest_number
       smallest_number = src[row_index][element_index]
       new_array << smallest_number
-      element_index += 1
     end
     element_index += 1
   end
+  row_index +=1
 end
 end
 
@@ -21,10 +21,6 @@ end
 # while row_index < spice_rack.count do
 #   element_index = 0
 #   while element_index < spice_rack[row_index].count do
-#     # How to read the following line of code:
-#     #   Array at row_index
-#     #   Element of the inner array at element_index
-#     #   The first character of that element...
 #     if spice_rack[row_index][element_index][0] == "P"
 #       outer_results << spice_rack[row_index][element_index]
 #     end
